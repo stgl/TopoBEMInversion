0c4688cc03b1916b042ef41e566a90e78fabb83f
George Hilley <gehilley@gmail.com>
2019-12-16 09:31:57 -0800

    Update topo_jacobian_K.m

--- src/topo_jacobian_K.m
+++ src/topo_jacobian_K.m
@@ -2,7 +2,7 @@ function J = topo_jacobian_K(K, e_chan, e_outlets, ...
     sig_elev, ind_chan_misfit, G_chan, Ginv_elev, bay_constr, ...
     w_bay_constr, G_bay, lp_constr, w_lp_constr, G_lp, geo_map)
 
-log_dK = 1E-3;
+log_dK = 1E-10;
 J = zeros(length(K),length(e_chan));
 
 % Calculate base d:
