73c91d0f8e63dee2c079d231f6af4b7cfdc979ab
George Hilley <gehilley@gmail.com>
2019-12-16 10:50:14 -0800

    Update multiK_strong_enforcement_linearized_cov.m

--- results/multiK_strong_enforcement_linearized_cov.m
+++ results/multiK_strong_enforcement_linearized_cov.m
@@ -1,4 +1,5 @@
 scenario = 'strong';
+n = 5000;
 
 % Setting the correct directories
 p=pathdef; path(p)
@@ -11,6 +12,21 @@ J = topo_jacobian_K(K, e_chan, e_outlets, ...
     sig_elev, ind_chan_misfit, G_chan, Ginv_elev, bay_constr, ...
     w_bay_constr, G_bay, lp_constr, w_lp_constr, G_lp, geo_map);
 
-varE = ones(length(K),1) * sig_elev.^2;
+covK = J * sig_elev.^2;
 
-sigK = J'*varE*J;
+% Draw random samples:
+samples = mvnrnd(K,covK,n);
+
+fun = @(K)topo_linear_lsq_model_cov(K, e_chan, e_outlets, ...
+    sig_elev, ind_chan_misfit, G_chan, Ginv_elev, bay_constr, ...
+    w_bay_constr, G_bay, lp_constr, w_lp_constr, G_lp, geo_map);
+v = zeros(length(samples), 5);
+for i = 1:length(samples(:,1))
+  [thisv, thiscov_v] = fun(ssamples(i,:));
+  v(i,1:2) = thisv;
+  v(i,3) = thiscov_v(1,1);
+  v(i,4) = thiscov_v(2,2);
+  v(i,5) = thiscov_v(1,2);
+end
+
+save multiK_mcmc_weak_enforcement_v_with_cov_linearized v
