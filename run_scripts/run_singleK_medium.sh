67b4a8b0b3df5e5b86cfd77358f255e3fc28552e
George Hilley <gehilley@gmail.com>
2020-09-15 07:08:49 -0700

    regeared remainder of run scripts.

--- run_scripts/run_singleK_medium.sh
+++ run_scripts/run_singleK_medium.sh
@@ -1,11 +1,11 @@
-matlab -nodesktop singleK_medium_enforcement_n0_667.m > singleK_medium_enforcement_n0_667.out; \
-matlab -nodesktop singleK_medium_enforcement_linearized_cov_n0_667.m > singleK_medium_enforcement_linearized_cov_n0_667.out; \
-matlab -nodesktop singleK_medium_enforcement_linearized_output_n0_667.m > singleK_medium_enforcement_linearized_output_n0_667.out &
+nohup bash -c "matlab -nodesktop < models/singleK_medium_enforcement_n0_667.m > results/singleK_medium_enforcement_n0_667.out; \
+matlab -nodesktop < models/singleK_medium_enforcement_linearized_cov_n0_667.m > results/singleK_medium_enforcement_linearized_cov_n0_667.out; \
+matlab -nodesktop < models/singleK_medium_enforcement_linearized_output_n0_667.m > results/singleK_medium_enforcement_linearized_output_n0_667.out;" > singleK_medium_n0_667.out &
 
-matlab -nodesktop singleK_medium_enforcement_n1_0.m > singleK_medium_enforcement_n1_0.out; \
-matlab -nodesktop singleK_medium_enforcement_linearized_cov_n1_0.m > singleK_medium_enforcement_linearized_cov_n1_0.out; \
-matlab -nodesktop singleK_medium_enforcement_linearized_output_n1_0.m > singleK_medium_enforcement_linearized_output_n1_0.out &
+nohup bash -c "matlab -nodesktop < models/singleK_medium_enforcement_n1_0.m > results/singleK_medium_enforcement_n1_0.out; \
+matlab -nodesktop < models/singleK_medium_enforcement_linearized_cov_n1_0.m > results/singleK_medium_enforcement_linearized_cov_n1_0.out; \
+matlab -nodesktop < models/singleK_medium_enforcement_linearized_output_n1_0.m > results/singleK_medium_enforcement_linearized_output_n1_0.out;" > singleK_medium_n1.out &
 
-matlab -nodesktop singleK_medium_enforcement_n2_5.m > singleK_medium_enforcement_n2_5.out; \
-matlab -nodesktop singleK_medium_enforcement_linearized_cov_n2_5.m > singleK_medium_enforcement_linearized_cov_n2_5.out; \
-matlab -nodesktop singleK_medium_enforcement_linearized_output_n2_5.m > singleK_medium_enforcement_linearized_output_n2_5.out &
+nohup bash -c "matlab -nodesktop < models/singleK_medium_enforcement_n2_5.m > results/singleK_medium_enforcement_n2_5.out; \
+matlab -nodesktop < models/singleK_medium_enforcement_linearized_cov_n2_5.m > results/singleK_medium_enforcement_linearized_cov_n2_5.out; \
+matlab -nodesktop < models/singleK_medium_enforcement_linearized_output_n2_5.m > results/singleK_medium_enforcement_linearized_output_n2_5.out; " > singleK_medium_n2_5.out &
