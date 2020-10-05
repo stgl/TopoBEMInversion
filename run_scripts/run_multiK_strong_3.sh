ab6f7fc1a64d5afcbe5015c030944bebb925dbd6
George Hilley <gehilley@gmail.com>
2020-10-05 07:37:39 -0700

    Made n=2 models and runscripts.

---
+++ run_scripts/run_multiK_strong_3.sh
@@ -0,0 +1,1 @@
+nohup bash -c "matlab -nodesktop < models/multiK_strong_enforcement_n2.m > results/multiK_strong_enforcement_n2.out;  " > multiK_strong_n2.out &
---
+++ run_scripts/run_singleK_medium_2.sh
@@ -0,0 +1,1 @@
+nohup bash -c "matlab -nodesktop < models/singleK_medium_enforcement_n2.m > results/singleK_medium_enforcement_n2.out;" > singleK_medium_n2.out &
