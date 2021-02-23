7a13da6b4fa1888a95b3ce4f1e2d637e95876a55
George Hilley <gehilley@gmail.com>
2021-02-23 12:08:00 -0800

    fixed run scripts.

--- run_scripts/run_multiK_medium.sh
+++ run_scripts/run_multiK_medium.sh
@@ -1,11 +1,15 @@
-nohup bash -c "matlab -nodesktop < models/multiK_medium_enforcement_n0_667.m > results/multiK_medium_enforcement_n0_667.out; \
-matlab -nodesktop < models/multiK_medium_enforcement_linearized_cov_n0_667.m > results/multiK_medium_enforcement_linearized_cov_n0_667.out; \
-matlab -nodesktop < models/multiK_medium_enforcement_linearized_output_n0_667.m > results/multiK_medium_enforcement_linearized_output_n0_667.out;" > multiK_medium_n0_667.out &
+#nohup bash -c "matlab -nodesktop < models/multiK_medium_enforcement_n0_667.m > results/multiK_medium_enforcement_n0_667.out; \
+#matlab -nodesktop < models/multiK_medium_enforcement_linearized_cov_n0_667.m > results/multiK_medium_enforcement_linearized_cov_n0_667.out; \
+#matlab -nodesktop < models/multiK_medium_enforcement_linearized_output_n0_667.m > results/multiK_medium_enforcement_linearized_output_n0_667.out;" > multiK_medium_n0_667.out &
 
 #nohup bash -c "matlab -nodesktop < models/multiK_medium_enforcement_n1_0.m > results/multiK_medium_enforcement_n1_0.out; \
 #%matlab -nodesktop < models/multiK_medium_enforcement_linearized_cov_n1_0.m > results/multiK_medium_enforcement_linearized_cov_n1_0.out; \
 #%matlab -nodesktop < models/multiK_medium_enforcement_linearized_output_n1_0.m > results/multiK_medium_enforcement_linearized_output_n1_0.out;" > multiK_medium_n1.out &
 
-nohup bash -c "matlab -nodesktop < models/multiK_medium_enforcement_n2_5.m > results/multiK_medium_enforcement_n2_5.out; \
-matlab -nodesktop < models/multiK_medium_enforcement_linearized_cov_n2_5.m > results/multiK_medium_enforcement_linearized_cov_n2_5.out; \
-matlab -nodesktop < models/multiK_medium_enforcement_linearized_output_n2_5.m > results/multiK_medium_enforcement_linearized_output_n2_5.out; " > multiK_medium_n2_5.out &
+#nohup bash -c "matlab -nodesktop < models/multiK_medium_enforcement_n2_5.m > results/multiK_medium_enforcement_n2_5.out; \
+#matlab -nodesktop < models/multiK_medium_enforcement_linearized_cov_n2_5.m > results/multiK_medium_enforcement_linearized_cov_n2_5.out; \
+#matlab -nodesktop < models/multiK_medium_enforcement_linearized_output_n2_5.m > results/multiK_medium_enforcement_linearized_output_n2_5.out; " > multiK_medium_n2_5.out &
+
+nohup bash -c "matlab -nodesktop < models/multiK_medium_enforcement_n0_667.m > results/multiK_medium_enforcement_n0_667.out;" > multiK_medium_n0_667.out &
+
+nohup bash -c "matlab -nodesktop < models/multiK_medium_enforcement_n2_5.m > results/multiK_medium_enforcement_n2_5.out;" > multiK_medium_n2_5.out &
